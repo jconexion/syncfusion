029952f57bf294df006175ff784627dbdedc93a9
jconexion <conexion@live.be>
2023-10-17 10:07:51 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {PivotViewModule} from "@syncfusion/ej2-angular-pivotview";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PivotViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
